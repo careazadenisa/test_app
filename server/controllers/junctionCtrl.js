774bfab6139a82160e1d6035cfd83496cd8226d9
careazadenisa <careaza.denisa@gmail.com>
2023-11-08 13:04:16 +0200

    new file

--- server/controllers/junctionCtrl.js
+++ server/controllers/junctionCtrl.js
@@ -1,7 +1,9 @@
 module.exports = db => {
     return {
       destroy: (req, res) => {
-        db.query(`DELETE FROM "Junction" WHERE id_person = ${req.params.idperson} AND id_car = ${idcar}`, 
+        const personId=req.params.idperson;
+        const carId=req.params.idcar;
+        db.query(`DELETE FROM "Junction" WHERE id_person = ${personId} AND id_car = ${carId}`, 
         { type: db.QueryTypes.DELETE })
          .then(() => {
           res.send({ success: true });
